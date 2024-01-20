c0b042ab87bbc4c37a8fd9e6b2fa862f9ada9ba2
rakibul12212 <rakibul15-12212@diu.edu.bd>
2024-01-20 12:18:59 -0800

    reusable layout

---
+++ src/components/layout/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="h-screen bg-red-500">
+      <h1>sidebar</h1>
+    </div>
+  );
+};
+
+export default Sidebar;
