a79699d5abf7d4292aef8e7931e05f5a3adba5f7
rakibul12212 <rakibul15-12212@diu.edu.bd>
2024-01-20 12:55:12 -0800

    tailwind css customizetion way 2

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import Container from "./components/ui/Container";
 function App() {
   return (
     <Container>
-      <h1>Main content</h1>
+      <div className="h-screen w-full flex justify-center items-center">
+        <button className="btn btn-primary">this is a button</button>
+        <button className="btn btn-danger">this is a button</button>
+      </div>
     </Container>
   );
 }
--- src/components/ui/Container.tsx
+++ src/components/ui/Container.tsx
@@ -1,11 +1,7 @@
 import React, { ReactNode } from "react";
 
 const Container = ({ children }: { children: ReactNode }) => {
-  return (
-    <div className="bg-primary-gradient h-full w-full max-w-[1250] mx-auto">
-      {children}
-    </div>
-  );
+  return <div className=" h-full w-full max-w-[1250] mx-auto">{children}</div>;
 };
 
 export default Container;
