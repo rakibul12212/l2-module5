8278c5b702fc087f4f173ae87883ee15521ffcb4
rakibul12212 <rakibul15-12212@diu.edu.bd>
2024-01-20 13:06:05 -0800

    tailwind css customizetion way 3

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ function App() {
   return (
     <Container>
       <div className="h-screen w-full flex justify-center items-center">
+        <h1>this is a h1 tag</h1>
+        <h2>this is a h2 tag</h2>
         <button className="btn btn-primary">this is a button</button>
         <button className="btn btn-danger">this is a button</button>
       </div>
