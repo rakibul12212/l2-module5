3ca7035e9e6e31c26bf02e4eadf316a01fcbdc4a
rakibul12212 <rakibul15-12212@diu.edu.bd>
2024-01-22 01:53:35 +0600

    complete reusable button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,20 +1,40 @@
-import { twMerge } from "tailwind-merge";
-import { clsx } from "clsx";
+import { ButtonHTMLAttributes, DetailedHTMLProps, forwardRef } from "react";
 import cn from "../../utils/cn";
 
-const Button = ({ className, variant }) => {
-  const getVariant = (variant) => {
-    switch (variant) {
-      case "outline":
-        return "btn-outline";
-      case "ghost":
-        return "btn-ghost";
-
-      default:
-        return "btn-solid";
-    }
-  };
-  return <button className={cn(getVariant(variant), className)}>click</button>;
+type TRef = HTMLButtonElement;
+type TVariant = "ghost" | "outline" | "solid";
+type TButtonOptions = {
+  variant?: TVariant;
 };
+type TButton = DetailedHTMLProps<
+  ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+> &
+  TButtonOptions;
+
+const Button = forwardRef<TRef, TButton>(
+  ({ className, variant = "solid"  , ...rest }, ref) => {
+    const getVariant = (variant: TVariant) => {
+      switch (variant) {
+        case "outline":
+          return "btn-outline";
+        case "ghost":
+          return "btn-ghost";
+
+        default:
+          return "btn-solid";
+      }
+    };
+    return (
+      <button
+        {...rest}
+        ref={ref}
+        className={cn(getVariant(variant), className)}
+      >
+        click
+      </button>
+    );
+  }
+);
 
 export default Button;
